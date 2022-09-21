fa9b3468fd1ff50d1a0e4fe008706353bef1f052
Kamil <kamilkarpinski98@gmail.com>
2022-09-21 15:55:48 +0200

    #1 commit: SessionRepo and Session works, MySQL db works

---
+++ src/main/java/pl/karpinski/LearnTimer/LearnTimerApplication.java
@@ -0,0 +1,13 @@
+package pl.karpinski.LearnTimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnTimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnTimerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/karpinski/LearnTimer/LearnTimerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.karpinski.LearnTimer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnTimerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
